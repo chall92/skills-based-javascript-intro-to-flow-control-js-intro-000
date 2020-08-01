d5802e3db78a48fc555865e38283fe2a6a9fdbea
Corbin Hall <hallcorbin92@gmail.com>
2020-08-01 05:38:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,7 +22,7 @@ if (age >=13 && age <=19) {
 else {
   return 'You are a kid'
 }
-}   
+}
 
 function ternaryTeenager(age) {
 return (age >=13 && age <=19) ? 'You are a teenager' : 'You are not a teenager'
