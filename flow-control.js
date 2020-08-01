2dfb907d9da4507b3070587b3383e9c436cb34dd
Corbin Hall <hallcorbin92@gmail.com>
2020-08-01 05:34:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,10 @@
 function basicTeenager(age) {
-if (age >= 13 && age <=19) {
+if (age >=13 && age <=19) {
 return 'You are a teenager!'}
 }
 
 function teenager(age) {
-  if (age >= 13 && age <=19) {
+  if (age >=13 && age <=19) {
     return 'You are a teenager!'
   }
 else {
@@ -25,7 +25,7 @@ else {
 }
 
 function ternaryTeenager(age) {
-return (age >= 13 && age <= 19) ? 'You are a teenager' : 'You are not a teenager'
+return (age >=13 && age <=19) ? 'You are a teenager' : 'You are not a teenager'
 }
 
 function switchAge(age) {
