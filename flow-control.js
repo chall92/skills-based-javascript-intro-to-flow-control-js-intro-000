1ad018caf9fcc232a62ebd18e22b3e7da3308fdd
Corbin Hall <hallcorbin92@gmail.com>
2020-08-01 04:46:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,5 +19,17 @@ return (age >= 13 && age <= 19) ? 'You are a teenager' : 'You are not a teenager
 }
 
 function switchAge(age) {
-
+switch(age) {
+  case (age >=13 && age <= 19):
+  'You are a teenager!';
+   break;
+  case (age > 19):
+  'You are a grownup';
+  break;
+ case (age < 13):
+ 'You are a kid';
+ break;
+ default:
+ 'You have an age'
+}
 }
